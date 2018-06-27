6f69d0434f864975361bda5418b256676c37338d
EC2 Default User <ec2-user@ip-172-31-18-52.us-west-2.compute.internal>
2018-06-27 19:03:09 +0000

    Delete instance still not working but dont want to lose it

--- tests/test_ec2.py
+++ tests/test_ec2.py
@@ -23,12 +23,16 @@ def list_instances():
     for instance in ec2.instances.all():
         print (instance.id, instance.state) 
         
-def terminate_instances():
-    instanceTerm = ec2.Instance(tempID)
+def terminate_instances(termID):
+    instanceTerm = ec2.Instance(termID)
     response = instanceTerm.terminate() 
-    print (response)
+    #print (response)
+
 
 #tests using moto from here
+
+
+#tests making an ec2 instance with the make_instance function
 @mock_ec2
 def test_new_instance():
     make_instance()
@@ -37,10 +41,23 @@ def test_new_instance():
     assert len(instances) == 1
     instance1 = instances[0]
     assert instance1['ImageId'] == 'ami-4e700e36'
-       
+
+
+#tests whether an instance is terminated after calling terminate_instances
+@mock_ec2
+def test_delete_instance():
+   tempec2 = boto3.resource('ec2')
+   make_instance()
+   terminate_instances(tempID)
+   for instance1 in tempec2.instances.all(): 
+        if tempID is instance1.id:
+            print ('reached')
+            assert instance.state == 'terminated' or 'shutting-down'
+
 if __name__ == '__main__':
     #make_instance()
     test_new_instance()
+    test_delete_instance()
     #list_instances()
     #print('=============')
     #terminate_instances()
