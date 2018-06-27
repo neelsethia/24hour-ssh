ff52acdefa3f5a630add99d85a5ea5d03edc7561
EC2 Default User <ec2-user@ip-172-31-18-52.us-west-2.compute.internal>
2018-06-27 17:08:31 +0000

    Added moto test for creating a new instance

--- tests/test_ec2.py
+++ tests/test_ec2.py
@@ -5,6 +5,7 @@ import botocore
 from botocore.exceptions import ClientError
 import string 
 import moto 
+from moto import mock_ec2
 
 ec2 = boto3.resource('ec2')
 
@@ -19,6 +20,32 @@ def make_instance():
     tempID = instance[0].id 
 
 def list_instances():
+    for instance in ec2.instances.all():
+        print (instance.id, instance.state) 
+        
+def terminate_instances():
+    instanceTerm = ec2.Instance(tempID)
+    response = instanceTerm.terminate() 
+    print (response)
+
+#tests using moto from here
+@mock_ec2
+def test_new_instance():
+    make_instance()
+    client = boto3.client('ec2',region_name='us-west-2')
+    instances = client.describe_instances()['Reservations'][0]['Instances']
+    assert len(instances) == 1
+    instance1 = instances[0]
+    assert instance1['ImageId'] == 'ami-4e700e36'
+       
+if __name__ == '__main__':
+    #make_instance()
+    test_new_instance()
+    #list_instances()
+    #print('=============')
+    #terminate_instances()
+    #print('=============')
+    #list_instances()
     
     
 
