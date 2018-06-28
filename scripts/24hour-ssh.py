08452f1059c50c0dd35c3af2016cc51a244dbfb7
EC2 Default User <ec2-user@ip-172-31-18-52.us-west-2.compute.internal>
2018-06-28 18:46:28 +0000

    revised

--- scripts/24hour-ssh.py
+++ scripts/24hour-ssh.py
@@ -3,5 +3,11 @@
 #Script which will prompt for username, target host, and the reason for access request. 
 #Script will generate temporary ssh keypair. 
 #Script will call ssm service to install generated ssh pubilc key into described file. 
-#Script will delete temporary ssh keypair from cloud9 instance after 24 hours. 
+#Script will delete temporary ssh keypair from cloud9 instance after 24 hours.  
+
+
+
+
+
+
 
