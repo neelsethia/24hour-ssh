49906441969c29ec3b0d3ebefa8dbf56e907182a
Ashley Gould <agould@ucop.edu>
2018-07-06 17:52:47 -0700

    adding tests for ssm parameter

--- tests/test_ssm.py
+++ tests/test_ssm.py
@@ -34,6 +34,79 @@ def ssm_send_command(instance_id, command_doc_name):
     )
     return response
 
+def ssm_set_encrypted_param(param_name, param_value, key_id=None):
+    ssm = boto3.client('ssm')
+    kwargs = dict(
+        Name=param_name,
+        Value=param_value,
+        Type='SecureString',
+        Overwrite=True,
+    )
+    if key_id:
+        kwargs['KeyId'] = key_id
+    response = ssm.put_parameter(**kwargs)
+    return response['Version']
+
+def ssm_get_encrypted_param(param_name):
+    ssm = boto3.client('ssm')
+    response = ssm.get_parameter(
+        Name=param_name,
+        WithDecryption=True,
+    )
+    return response
+
+def ssm_set_list_param(param_name, param_value_list):
+    ssm = boto3.client('ssm')
+    value = ','.join(param_value_list)
+    response = ssm.put_parameter(
+        Name=param_name,
+        Value=','.join(param_value_list),
+        Type='StringList',
+        Overwrite=True,
+    )
+    return response
+
+def ssm_get_param(param_name):
+    ssm = boto3.client('ssm')
+    response = ssm.get_parameter(
+        Name=param_name,
+    )
+    return response
+
+
+
+param_name = 'passwd'
+param_value = 'doiu*(.3szjk'
+
+@mock_ssm
+def test_ssm_set_param():
+    response = ssm_set_encrypted_param(param_name, param_value)
+    print(response)
+    assert isinstance(response, int)
+
+@mock_ssm
+def test_ssm_get_param():
+    ssm_set_encrypted_param(param_name, param_value)
+    response = ssm_get_encrypted_param(param_name)
+    print(response)
+    assert response['Parameter']['Value'] == param_value
+
+
+param_name = 'MyStruggle'
+param_value_list = [
+    'today I suffer',
+    'under extreem duress',
+    'the ineptitude of my peers',
+    'chmod 777 for crying out loud',
+]
+@mock_ssm
+def test_ssm_list_param():
+    ssm_set_list_param(param_name, param_value_list)
+    response = ssm_get_param(param_name)
+    print(response['Parameter']['Value'])
+    assert response['Parameter']['Value'] == param_value_list.split(',')
+
+
 
 @mock_ec2
 @mock_ssm
