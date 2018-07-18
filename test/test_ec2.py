20bbb498e517c6f638614d2f75a15c919518dfce
Ashley Gould <agould@ucop.edu>
2018-07-17 19:15:31 -0700

    test_ec2.py: tests passeing again

--- test/test_ec2.py
+++ test/test_ec2.py
@@ -7,24 +7,25 @@ import string
 import moto 
 from moto import mock_ec2
 
-ec2 = boto3.resource('ec2',region_name='us-west-2' )
+#ec2 = boto3.resource('ec2',region_name='us-west-2' )
 
 def make_instance():
+    ec2= boto3.resource('ec2',region_name='us-west-2' )
     instance = ec2.create_instances(
         ImageId='ami-4e700e36', #this is for US-WEST-2 ebs-ssd
         MinCount=1,
         MaxCount=1,
         InstanceType='t1.micro' #t1.micro only works with EBS-SSD 
         )
-    global tempID
-    tempID = instance[0].id 
     return instance[0].id
 
 def list_instances():
+    ec2 = boto3.resource('ec2',region_name='us-west-2' )
     for instance in ec2.instances.all():
         print (instance.id, instance.state) 
         
 def terminate_instances(termID):
+    ec2 = boto3.resource('ec2',region_name='us-west-2' )
     instanceTerm = ec2.Instance(termID)
     response = instanceTerm.terminate() 
     #print (response)
@@ -47,9 +48,9 @@ def test_new_instance():
 #tests whether an instance is terminated after calling terminate_instances
 @mock_ec2
 def test_delete_instance():
-   tempec2 = boto3.resource('ec2', region_name='us-west-2')
-   make_instance()
+   tempID = make_instance()
    terminate_instances(tempID)
+   tempec2 = boto3.resource('ec2', region_name='us-west-2')
    for instance1 in tempec2.instances.all(): 
         if tempID is instance1.id:
             print ('reached')
