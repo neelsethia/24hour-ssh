e82537b1be122dc41d93ddc6d15c8245fd9e5d52
Neel Sethia <neelsethia@gmail.com>
2018-07-23 20:45:09 +0000

    Updated further layout

---
+++ samples/ssm.py
@@ -0,0 +1,90 @@
+import os
+import boto3
+import json
+
+
+def read_public_key_from_file(pubkey_file):
+    with open(pubkey_file, 'r') as pubkey:
+        return pubkey.read()
+
+
+def get_ssm_client():
+    return boto3.client('ssm', 'us-west-2', aws_access_key_id="", aws_secret_access_key="")
+
+
+def build_document(pubkey):
+    document = dict(
+        schemaVersion='2.2',
+        description='Install ssh public key into ~ec2-user/.ssh/authorized_keys',
+        parameters=dict(),
+        mainSteps=[
+            dict(
+                action='aws:runShellScript',
+                name='sshPubkeySetup',
+                inputs=dict(
+                    runCommandand=[
+                        'SSH_PUBKEY="{}"'.format(pubkey),
+                        'echo $SSH_PUBKEY >> ~ec2-user/.ssh/authorized_keys',
+                    ]
+                )
+            )
+        ]
+    )
+    return json.dumps(document)
+
+
+def upload_document(client, document_name, document):
+    try:
+        client.create_document(
+            Content=document,
+            Name='sshPubkeySetup',
+            DocumentType='Command',
+            DocumentFormat='JSON',
+            TargetType='/AWS::EC2::Instance'
+        )
+    except client.exceptions.DocumentAlreadyExists:
+        pass
+    return
+
+
+def send_command(client, instance_id):
+    response = client.send_command(
+        InstanceIds=[instance_id],
+        DocumentName='sshPubkeySetup',
+        TimeoutSeconds=3600,
+        Parameters=dict(),
+    )
+    return response
+
+
+def is_24hourssh_enabled(instance):
+    is_enabled = next((
+            True for tag in instance.tags
+            if tag['Key'] == '24hourssh'
+            and tag['Value'] == 'enabled'
+        ), False)
+    return is_enabled
+
+
+def get_24hourssh_enabled_instances():
+    ec2 = boto3.resource('ec2', region_name='us-west-2')
+    return [
+        instance for instance in ec2.instances.all()
+        if instance.state['Name'] == 'running'
+        and is_24hourssh_enabled(instance)
+    ]
+
+
+def main():
+    client = boto3.client('ssm', 'us-west-2')
+    pubkey_file = '~/.ssh/id_rsa.pub'
+    pubkey = read_public_key_from_file(os.path.expanduser(pubkey_file))
+    document = build_document(pubkey)
+    document_name = 'sshPubkeySetup'
+    upload_document(client, document_name, document)
+    for instance in get_24hourssh_enabled_instances():
+        print(instance.id)
+
+
+if __name__ == '__main__':
+    main()
---
+++ samples/test_ec2.py
@@ -0,0 +1,87 @@
+#!/bin/env python 
+import sys 
+import os
+import boto3 
+import botocore
+from botocore.exceptions import ClientError
+import string 
+import moto 
+from moto import mock_ec2
+
+#working from ec2 user
+ec2 = boto3.resource('ec2', 'us-west-2', aws_access_key_id="", aws_secret_access_key="")
+
+def make_instance():
+    ec2= boto3.resource('ec2',region_name='us-west-2' )
+    instance = ec2.create_instances(
+        ImageId='ami-4e700e36', #this is for US-WEST-2 ebs-ssd
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t1.micro' #t1.micro only works with EBS-SSD 
+        )
+    return instance[0].id
+
+def list_instances():
+    ec2 = boto3.resource('ec2',region_name='us-west-2' )
+    for instance in ec2.instances.all():
+        print (instance.id, instance.state) 
+        
+def terminate_instances(termID):
+    ec2 = boto3.resource('ec2',region_name='us-west-2' )
+    instanceTerm = ec2.Instance(termID)
+    response = instanceTerm.terminate() 
+    #print (response)
+
+
+#tests using moto from here
+
+
+#tests making an ec2 instance with the make_instance function
+@mock_ec2
+def test_new_instance():
+    make_instance()
+    client = boto3.client('ec2',region_name='us-west-2')
+    instances = client.describe_instances()['Reservations'][0]['Instances']
+    assert len(instances) == 1
+    instance1 = instances[0]
+    assert instance1['ImageId'] == 'ami-4e700e36'
+
+
+#tests whether an instance is terminated after calling terminate_instances
+@mock_ec2
+def test_delete_instance():
+   #tempec2 = boto3.resource('ec2')
+   
+   print("==============")
+   #because data is not persistent between moto tests, reimplement test_new_instance
+   checkID = make_instance()
+
+   list_instances()
+   print("==============")
+   print ("checking to see if checkID exists: " + checkID)
+  
+   terminate_instances(checkID)
+   
+   for instance1 in ec2.instances.all(): 
+        if checkID == instance1.id:
+            #wired together weirdly but it works kind of 
+            stateChecker = str(instance1.state)
+            print('instance state: ' + stateChecker)
+            assert stateChecker == "{\'Code\': 48, \'Name\': \'terminated\'}" or stateChecker == "{\'Name\': \'terminated\', \'Code\': 48}"
+                
+if __name__ == '__main__':
+    #make_instance()
+
+    #list_instances()
+    #print('=============')
+    #terminate_instances()
+    #print('=============')
+
+    
+    #test_new_instance()
+    test_delete_instance()
+    #print('=============')
+    #list_instances()
+    
+
+
