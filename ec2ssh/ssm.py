1ba369c1076471f73eb4be193351de68eb843512
Neel Sethia <neelsethia@gmail.com>
2018-07-26 22:00:22 +0000

    Moving stuff

--- ec2ssh/ssm.py
+++ ec2ssh/ssm.py
@@ -54,36 +54,6 @@ def send_command(client, instance_id):
     )
     return response
 
-def is_24hourssh_enabled(instance):
-    is_enabled = next((
-            True for tag in instance.tags
-            if tag['Key'] == '24hourssh'
-            and tag['Value'] == 'enabled'
-        ), False)
-    return is_enabled
-
-def get_24hourssh_enabled_instances():
-    ec2 = boto3.resource('ec2', region_name='us-west-2')
-    return [
-        instance for instance in ec2.instances.all()
-        if instance.state['Name'] == 'running'
-        and is_24hourssh_enabled(instance)
-    ]
-
-def compare_24hourssh_enabled_instance_ID(instanceID):
-    ec2=boto3.resource('ec2',region_name='us-west-2')
-    for instance in ec2.instances.all():
-        if (instance.id == instanceID 
-        and instance.state['Name'] == 'running' 
-        and is_24hourssh_enabled(instance)):
-                return True
-    return False
-    
-def test_output():
-    ec2=boto3.resource('ec2',region_name='us-west-2')
-    for instance in ec2.instances.all():
-        if is_24hourssh_enabled(instance):
-            print(instance.id) 
 
 def main():
     client = boto3.client('ssm', 'us-west-2')
@@ -92,8 +62,8 @@ def main():
     document = build_document(pubkey)
     document_name = 'sshPubkeySetup'
     upload_document(client, document_name, document)
-    for instance in get_24hourssh_enabled_instances():
-        print(instance.id)
+    # for instance in get_24hourssh_enabled_instances():
+    #     print(instance.id)
 
     
 
